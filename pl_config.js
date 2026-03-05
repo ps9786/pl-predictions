487f1befc5a1687574ce4fff06983d49a95e0154
paul <paul@mint-laptop.local>
2026-03-05 19:37:18 +0000

    Removed js file

--- pl_config.js
+++
@@ -1,5 +0,0 @@
-// Pushover API credentials
-// Get your token at: https://pushover.net/apps/build
-// Get your user key at: https://pushover.net (shown on dashboard)
-const PUSHOVER_TOKEN = 'a2paeq3y8jtwgad72yab426od3yq3t';
-const PUSHOVER_USER  = 'TstHIUA38ddpjefiGYPrMZxlramzrR';

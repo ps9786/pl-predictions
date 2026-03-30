0036d4cabcdc8b87799f519cc3e8432c431be289
Paul Seward <pseward@gmail.com>
2026-03-30 21:15:14 +0100

    Added to Precheck

--- copy_files.sh
+++
@@ -1,8 +0,0 @@
-#cp ../ClaudeCode/plp.html ./plp.html
-#cp ../ClaudeCode/plp.html ./plpb.html
-cp ../football-predictions/matchup_summary.csv .
-cp ../football-predictions/team_form_summary.csv .
-cd ../football-predictions/
-bash ./make_games_text.sh
-cd ../pl-predictions
-cp ../football-predictions/games.txt .

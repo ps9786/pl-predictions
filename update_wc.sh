a880fb137095d9d60e22cbe19435720affbd5eec
Paul Seward <pseward@gmail.com>
2026-06-17 13:43:18 +0100

    New day

--- update_wc.sh
+++ update_wc.sh
@@ -12,6 +12,7 @@ fi
 cd ~/vscode/world-cup-2026-selections-explorer
 curl https://fixturedownload.com/download/fifa-world-cup-2026-UTC.csv -o ./public/wc2026.csv
 curl https://raw.githubusercontent.com/openfootball/worldcup.json/refs/heads/master/2026/worldcup.json -o ./public/worldcup.json
+python3 calculate_league_table.py --output public/league_table.csv
 npm run build
 cp -a dist/wc/* ~/vscode/pl-predictions/wc
 cd ~/vscode/pl-predictions

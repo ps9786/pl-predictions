0b18d9375c8e1ee6ae0a9c076eeb3129f0a040ec
Paul Seward <pseward@gmail.com>
2026-06-17 14:59:32 +0100

    Updated update_wc.sh

--- update_wc.sh
+++ update_wc.sh
@@ -2,8 +2,8 @@ cd ~/vscode/world-cup-2026-selections-explorer
 git pull
 cd ~/vscode/pl-predictions
 git pull
-cd ~/Downloads
-if [ -f world-cup-2026-selections-explorer.zip ]; then
+if [ -f ~/Downloads/world-cup-2026-selections-explorer.zip ]; then
+  cd ~/Downloads
   cp world-cup-2026-selections-explorer.zip ~/vscode/world-cup-2026-selections-explorer
   rm world-cup-2026-selections-explorer.zip
   cd ~/vscode/world-cup-2026-selections-explorer
@@ -13,11 +13,12 @@ cd ~/vscode/world-cup-2026-selections-explorer
 curl https://fixturedownload.com/download/fifa-world-cup-2026-UTC.csv -o ./public/wc2026.csv
 curl https://raw.githubusercontent.com/openfootball/worldcup.json/refs/heads/master/2026/worldcup.json -o ./public/worldcup.json
 python3 calculate_league_table.py --output public/league_table.csv
+rm -fr ~/vscode/pl-predictions/wc/*
 rm -fr dist/
 npm run build
-rm -fr ~/vscode/pl-predictions/wc/*
 cp -a dist/wc/* ~/vscode/pl-predictions/wc
 cd ~/vscode/pl-predictions
 git add .
-git commit -m "New day"
+NOW=$(date)
+git commit -m "New update: ${NOW}"
 git push

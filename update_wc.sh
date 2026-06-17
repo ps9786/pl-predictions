3185e0780025c76184153a6c63afff072d8ade21
Paul Seward <pseward@gmail.com>
2026-06-17 09:32:49 +0100

    Changed Update Script

--- update_wc.sh
+++ update_wc.sh
@@ -1,12 +1,15 @@
+cd ~/vscode/world-cup-2026-selections-explorer
+git pull
 cd ~/vscode/pl-predictions
 git pull
 cd ~/Downloads
-if [ ! -f world-cup-2026-selections-explorer.zip ]; then
-  cp world-cup-2026-selections-explorer.zip ~/vscode/wc
+if [ -f world-cup-2026-selections-explorer.zip ]; then
+  cp world-cup-2026-selections-explorer.zip ~/vscode/world-cup-2026-selections-explorer
   rm world-cup-2026-selections-explorer.zip
-  cd ~/vscode/wc
+  cd ~/vscode/world-cup-2026-selections-explorer
   unzip -o world-cup-2026-selections-explorer.zip
 fi
+cd ~/vscode/world-cup-2026-selections-explorer
 curl https://fixturedownload.com/download/fifa-world-cup-2026-UTC.csv -o ./public/wc2026.csv
 curl https://raw.githubusercontent.com/openfootball/worldcup.json/refs/heads/master/2026/worldcup.json -o ./public/worldcup.json
 npm run build

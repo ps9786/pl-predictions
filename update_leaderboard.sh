263cc752388eeef5c33ebc5a7c515035fc40f8c8
Paul Seward <pseward@gmail.com>
2026-06-18 08:00:04 +0100

    New update: Thu Jun 18 08:00:04 BST 2026

---
+++ update_leaderboard.sh
@@ -0,0 +1,30 @@
+cd ~/vscode/world-cup-2026-selections-explorer
+git pull
+cd ~/vscode/pl-predictions
+git pull
+if [ -f ~/Downloads/world-cup-2026-selections-explorer.zip ]; then
+  cd ~/Downloads
+  cp world-cup-2026-selections-explorer.zip ~/vscode/world-cup-2026-selections-explorer
+  rm world-cup-2026-selections-explorer.zip
+  cd ~/vscode/world-cup-2026-selections-explorer
+  unzip -o world-cup-2026-selections-explorer.zip
+  git add .
+  NOW=$(date)
+  git commit -m "New update of world-cup-2026-selections-explorer: ${NOW}"
+  git push
+fi
+cd ~/vscode/world-cup-2026-selections-explorer
+curl https://fixturedownload.com/download/fifa-world-cup-2026-UTC.csv -o ./public/wc2026.csv
+curl https://raw.githubusercontent.com/openfootball/worldcup.json/refs/heads/master/2026/worldcup.json -o ./public/worldcup.json
+python3 calculate_league_table.py --output public/league_table.csv
+rm -fr ~/vscode/pl-predictions/wc/*
+rm -fr dist/
+npm run build
+cp -a dist/wc/* ~/vscode/pl-predictions/wc
+cd ~/vscode/pl-predictions
+date +%d > wc/current_date.txt
+git add .
+NOW=$(date)
+git commit -m "New update: ${NOW}"
+git push
+

68ca68763650f9c103ad2824b517ed56a6e08e5a
Paul Seward <pseward@gmail.com>
2026-06-18 08:33:30 +0100

    New Leaderboard: Thu 18 Jun 2026 08:33:30 BST

--- update_leaderboard.sh
+++ update_leaderboard.sh
@@ -21,6 +21,7 @@
 #npm run build
 #cp -a dist/wc/* ~/vscode/pl-predictions/wc
 cd ~/vscode/knipey
+curl https://raw.githubusercontent.com/openfootball/worldcup.json/refs/heads/master/2026/worldcup.json -o ./wc/worldcup.json
 curl https://fixturedownload.com/download/fifa-world-cup-2026-UTC.csv -o ./public/wc2026.csv
 python3 ./calculate_league_table.py --output ~/vscode/pl-predictions/wc/league_table.csv
 cd ~/vscode/pl-predictions

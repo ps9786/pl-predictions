60afdf124b87954763f5503d7686a09dd937de0e
Paul Seward <pseward@gmail.com>
2026-06-25 20:06:39 +0100

    New Predictions

--- tools/make_wc_data.sh
+++ tools/make_wc_data.sh
@@ -2,7 +2,7 @@ cd ~/vscode/pl-predictions/tools
 python ./world_cup_predictions.py 
 python ./quirky.py
 python ./world_cup_standings.py
-python ./world_up_next_7_days.py > ../wc/next_7_days.json
+python ./world_up_next_7_days.py
 mv worldcup_quirky.json ../wc/
 mv wc2026_predictions.json ../wc/
 mv wc2026_standings.json ../wc/

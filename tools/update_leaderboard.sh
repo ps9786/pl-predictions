1912116e934589aae313a9bb6f52d8cdf9477e7f
Paul Seward <pseward@gmail.com>
2026-06-28 21:50:33 +0100

    Round of 32

--- tools/update_leaderboard.sh
+++ tools/update_leaderboard.sh
@@ -1,11 +1,11 @@
 cd ~/vscode/pl-predictions
 curl https://raw.githubusercontent.com/openfootball/worldcup.json/refs/heads/master/2026/worldcup.json -o ./wc/worldcup.json
-cd ~/vscode/knipey
+cd ~/vscode/calculate_leaderboard
 curl https://fixturedownload.com/download/fifa-world-cup-2026-UTC.csv -o ./public/wc2026.csv
 python3 ./calculate_league_table.py --output ~/vscode/pl-predictions/wc/league_table.csv
 cd ~/vscode/pl-predictions
 date +%d > ./wc/current_date.txt
-cp ~/vscode/knipey/public/wc2026.csv ./wc/wc2026.csv
+cp ~/vscode/calculate_leaderboard/public/wc2026.csv ./wc/wc2026.csv
 git add .
 NOW=$(date)
 git commit -m "New Leaderboard: ${NOW}"

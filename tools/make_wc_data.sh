11b6f6a77534f9fa1f29f98d482c222ae597f078
Paul Seward <pseward@gmail.com>
2026-06-30 13:09:00 +0100

    New Predictions

--- tools/make_wc_data.sh
+++ tools/make_wc_data.sh
@@ -5,8 +5,6 @@ python ./world_cup_standings.py
 python ./world_up_next_7_days.py
 mv worldcup_quirky.json ../wc/
 mv wc2026_predictions.json ../wc/
-mv wc2026_standings.json ../wc/
-./update_leaderboard.sh
 cd ..
 git add .
 git commit -m "New Predictions"
